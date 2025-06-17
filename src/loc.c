014661134618b5b79996d841eb935760ce237b91
Zffu <103074097+Zffu@users.noreply.github.com>
2025-06-17 16:55:08 +0200

    fix: typo of type

--- src/loc.c
+++ src/loc.c
@@ -6,7 +6,8 @@ asmlocation_t* sprintasm_locregister(uint8_t reg) {
     asmlocation_t* loc = malloc(sizeof(asmlocation_t));
     
     loc->type = REGISTER;
-    loc->bytes = (uint8_t){reg};
+    loc->bytes = malloc(1);
+    loc->bytes[0] = reg;
 
     return loc;
 }
@@ -15,8 +16,13 @@ asmlocation_t* sprintasm_locaddr(uint32_t addr) {
     asmlocation_t* loc = malloc(sizeof(asmlocation_t));
     uint8_t* b = (uint8_t*) &addr;
 
-    loc->type = REGISTER;
-    loc->bytes = (uint8_t*){b[0], b[0], b[2], b[3]};
+    loc->type = DIRECT_ADDRESS;
+    loc->bytes = malloc(4);
+
+    loc->bytes[0] = b[0];
+    loc->bytes[1] = b[1];
+    loc->bytes[2] = b[2];
+    loc->bytes[3] = b[3];
 
     return loc;
 }
@@ -25,7 +31,8 @@ asmlocation_t* sprintasm_locregaddr(uint8_t reg) {
     asmlocation_t* loc = malloc(sizeof(asmlocation_t));
     
     loc->type = ADDRESS_REGISTER;
-    loc->bytes = (uint8_t*) {reg};
+    loc->bytes = malloc(1);
+    loc->bytes[0] = reg;
 
     return loc;
 }
