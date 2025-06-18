2cdd762f6028fd56468509c9e0847ae0c902cf22
Zffu <103074097+Zffu@users.noreply.github.com>
2025-06-18 10:28:27 +0200

    push

--- tests/modrm.c
+++ tests/modrm.c
@@ -4,14 +4,18 @@
 
 #include <sprintasm/x86/modrm.h>
 #include <sprintasm/x86/loc.h>
+#include <sprintasm/x86/reg.h>
+
 
 #include <stdio.h>
 
 int main() {
     asmlocation_t* target = sprintasm_locaddr(454768455);
+    asmlocation_t* reg = sprintasm_locregister(REGISTER_ESP);
     
     int size = 0;
-    uint8_t* buff = sprintasm_modrmmake(REGISTER_EDX, target, &size);
+
+    uint8_t* buff = sprintasm_modrmmake(REGISTER_EDX, reg, &size);
     
     for(int i = 0; i < size; ++i) {
         printf("0x%x ", buff[i]);
